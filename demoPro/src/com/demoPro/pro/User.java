6ca545200f316be2d576cbf7fa072bcbf9d098ff
wangbin <wangbin@gmail.com>
2011-07-27 10:34:08 +0800

    changed User.java version 2.0

--- demoPro/src/com/demoPro/pro/User.java
+++ demoPro/src/com/demoPro/pro/User.java
@@ -2,10 +2,21 @@ package com.demoPro.pro;
 
 public class User {
 	
+	// 姓名
 	private String name;
 	
+	// 电话
+	private String cell;
+	
+	/**
+	 * 默认构造
+	 */
 	public User(){}
 	
+	/**
+	 * 有参构造
+	 * @param name
+	 */
 	public User(String name){
 		this.name = name;
 	}
@@ -16,6 +27,14 @@ public class User {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	public String getCell() {
+		return cell;
+	}
+
+	public void setCell(String cell) {
+		this.cell = cell;
 	}
 	
 }
